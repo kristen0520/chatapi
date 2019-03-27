736fa3b6766ac53888d98f862571c1018a674951
kristen <kristenmay0520@gmail.com>
2019-03-27 13:59:12 -0400

    messages data structure update in user model

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
   profilePicture: String,
   messages: {type: Array, default: [
     {sender:"Kristen", timestamp: Number(Date.now()),
-      messages: {text:"Welcome to my Chat App!", timestamp:Number(Date.now())}
+      messages: [{text:"Welcome to my Chat App!", timestamp:Number(Date.now())}]
     }
     ]}
 });
